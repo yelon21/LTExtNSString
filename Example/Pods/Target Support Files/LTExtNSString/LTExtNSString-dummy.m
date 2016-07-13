7a60e469dd136a78bc3be15c547cfbacf92d8b21
yelon21 <254956982@qq.com>
2016-07-13 22:19:26 +0800

    1st add

---
+++ Example/Pods/Target Support Files/LTExtNSString/LTExtNSString-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_LTExtNSString : NSObject
+@end
+@implementation PodsDummy_LTExtNSString
+@end
---
+++ Example/Pods/Target Support Files/LTExtNSString/LTExtNSString-umbrella.h
@@ -0,0 +1,12 @@
+#import <UIKit/UIKit.h>
+
+#import "NSString+html.h"
+#import "NSString+LTEncryption.h"
+#import "NSString+LTJson.h"
+#import "NSString+LTLocalizedString.h"
+#import "NSString+LTRex.h"
+#import "NSString+LTTransform.h"
+
+FOUNDATION_EXPORT double LTExtNSStringVersionNumber;
+FOUNDATION_EXPORT const unsigned char LTExtNSStringVersionString[];
+
